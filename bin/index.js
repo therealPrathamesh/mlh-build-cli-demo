c6f5ab907de3f60e666dead552b40f575735239d
therealPrathamesh <prabhu.prathamesh01@gmail.com>
2022-01-15 14:28:15 +0530

    Add chalk and boxen

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,22 @@
 #!/usr/bin/env node
 
+const chalk = require('chalk');
+const boxen = require('boxen');
+
+const name = challk.blue('Name') + ':' + chalk.green('Prathamesh Prbahu');
+
+const github = challk.blue('Github') + ':' + chalk.green('https://github.com/therealPrathamesh');
+
+const box = {
+    padding: 1,
+    margin: 1,
+    borderStyle: 'round',
+    borderColor: 'green',
+};
+
+const message = boxen(`${name}\${github}`, box);
+
+
 console.log(`Name: Prathamesh Prabhu
     Github:https://github.com/therealPrathamesh`
 );
